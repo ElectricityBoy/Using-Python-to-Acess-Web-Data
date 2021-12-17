0b1857fe01e50c9970affebea68fe6c550582260
Elder Pereira <elder-guedes@hotmail.com>
2021-12-17 11:30:41 -0300

    Week 3 finished

--- debugging.py
+++ debugging.py
@@ -1,3 +1,5 @@
+import re
 x = 'From: Using the : character'
 y = re.findall('^F.+:', x)
+#In this regular expression, it will find the phrase who start with F and ends with :
 print(y)
--- extracting.py
+++ extracting.py
@@ -1,6 +1,9 @@
 line = 'From elder.guedes@hotmail.com dez-15-2020'
 
 words = line.split()
+#When we use the split method, it will create a list of words that contain the words
 print(words)
+
 email = words[1].split('@')
+#When we use the split method, it will create a list of words that contain parts separated by what you define 
 print(email[1])
